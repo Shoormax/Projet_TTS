d2796be7ccf2ed32520baa6f875e29dffdff1514
thomas <thomas.vincentbtsnir@gmail.com>
2017-01-17 14:47:24 +0100

    Merge branch 'master' of https://github.com/Shoormax/Projet_TTS into thomas_V2
    # Conflicts:
    #	manifest.json
    #	script/script.js

--- script/script.js
+++ script/script.js
@@ -160,6 +160,8 @@ $(function () {
             });
         });
     }
+    $(window).on('beforeunload',annyangChargementPage());
+    controleVocale();
 });
 
 /**
@@ -208,4 +210,42 @@ function existe(variable, defaultVal)
     }
 
     return variable == defaultVal;
+}
+
+/**
+ *
+ */
+function controleVocale() {
+annyang.debug();
+    if(annyang) {
+       var command = {
+         "recherche *key" : function (key) {
+             window.open("https://www.google.fr/search?q="+key);
+             console.log(key);
+             responsiveVoice.speak("vous êtes sur la page des résultats google "+key,"French Female");
+
+         },
+          "page" : function () {
+
+             responsiveVoice.speak($(document).attr('title'),"French Female");
+
+         }
+
+       };
+
+    }
+    annyang.setLanguage("FR-fr");
+    annyang.addCommands(command);
+    annyang.start();
+
+}
+
+function annyangChargementPage() {
+    $(window).bind('beforeunload', function () {
+        annyang.abort();
+    });
+    window.onload = function () {
+        annyang.start();
+    }
+    
 }
